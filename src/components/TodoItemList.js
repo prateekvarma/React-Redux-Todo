59b639b65dfb7219f47410e0595698b0a166fb5e
Prateek Varma <prtk.varma@gmail.com>
2022-07-08 13:01:14 -0600

    initial setup, and created Todolist and TodoItemList components

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoItemList = () => {
+  return <div>TodoListItem</div>;
+};
+
+export default TodoItemList;
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => {
+        return <TodoItemList todo={todo} />;
+      })}
+    </div>
+  );
+};
