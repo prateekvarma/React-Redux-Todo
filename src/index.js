c1e1bd7f441961cd4029479f72fa5ec3a4718a25
Prateek Varma <prtk.varma@gmail.com>
2022-07-10 12:54:56 -0600

    Added PERSISTANCE to Redux, imported packages and changed all necessary configurations. *working persistance*

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { Provider } from "react-redux";
 
 import { configStore } from "./store";
 import App from "./components/App";
 
+const store = configStore();
+const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={configStore()}>
-      <App />
+    <Provider store={store}>
+      {/* loading here is to show something while the view loads */}
+      <PersistGate persistor={persistor} loading={<h3>Loading...</h3>}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
 import { createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
 import { todos } from "./reducers";
 
 const reducers = {
   todos: todos,
 };
 
-const rootReducer = combineReducers(reducers);
+//This is just an object that defines how and where to save the persisted data
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
-export const configStore = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configStore = () => createStore(persistedReducer); //persistedReducer instead of rootReducer when configuring for persistance
